4bbf3c33bc68d9ae924d7513210e97caf68d39e7
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-05 13:10:37 -0700

    added the rest of the calculator function and worte the tests for them.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,18 @@
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+};
 
+console.log(calculator.add(1, 1));
+
+module.exports = calculator;
--- testingPractice.test.js
+++ testingPractice.test.js
@@ -17,3 +17,12 @@ test('reverse String', () => {
 test('calculate add', () => {
   expect(calculator.add(2, 2)).toBe(4);
 });
+test('calculate subtract', () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
+test('calculate divide', () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+});
+test('calculate multiply', () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+});
