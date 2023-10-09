982232a755de4b9a4d8859600e6a47a37713a252
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-09 15:10:36 -0700

    I got the function to start working, but I don't know how to test it

--- analyzeArray.js
+++ analyzeArray.js
@@ -3,3 +3,30 @@
 //min = smallest number
 //max = largest number
 //length = array.length
+
+function analyze(array) {
+  let sum = 0;
+  const sortedArray = array.sort((a, b) => a - b);
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    sum += sortedArray[i];
+  }
+
+  const average = sum / array.length;
+  const min = sortedArray[0];
+  const max = sortedArray[sortedArray.length - 1];
+
+  const length = array.length;
+
+  const object = {
+    average,
+    min,
+    max,
+    length,
+  };
+
+  console.log(object);
+  return object;
+}
+
+analyze([1, 8, 3, 4, 2, 6]);
